6e0bb1c293555f97c330d7e14ae3ff05399d0532
Ryan Albon <ryan@honkmobile.com>
2014-08-13 14:02:37 -0400

    Adding hasProperty function

--- src/predicate.js
+++ src/predicate.js
@@ -14,6 +14,7 @@
       any: any,
       contains: contains,
       equals: equals,
+      hasProperty: hasProperty,
       is: is,
       isFalse: isFalse,
       isIn: isIn,
@@ -66,6 +67,12 @@
   function equals(value) {
     return function (candidate) {
       return value == candidate;
+    };
+  }
+
+  function hasProperty(property) {
+    return function (obj) {
+      return property in obj;
     };
   }
 
