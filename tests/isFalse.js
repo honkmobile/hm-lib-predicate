131db4fe4e486efa3c36daa00443321814c5e286
Ryan Albon <ryan@honkmobile.com>
2014-08-16 23:15:51 -0400

    More tests for isFalse

--- tests/isFalse.js
+++ tests/isFalse.js
@@ -9,10 +9,36 @@ describe('hm.lib.predicate.Predicate', function () {
   describe('isFalse', function () {
     it('should return true when given false', function () {
       expect(_isFalse(false)).toBe(true);
+      expect(_isFalse(Boolean(false))).toBe(true);
     });
 
     it('should return false when given true', function () {
       expect(_isFalse(true)).toBe(false);
+    });
+
+    it('should return false when given any number', function () {
+      var i;
+
+      for (i = 0; i < 5000; i++) {
+        expect(_isFalse(Math.random())).toBe(false);
+        expect(_isFalse(-Math.random())).toBe(false);
+      }
+
+      expect(_isFalse(0)).toBe(false);
+      expect(_isFalse(NaN)).toBe(false);
+    });
+
+    it('should return false when given any string', function () {
+      expect(_isFalse('')).toBe(false);
+      expect(_isFalse(' ')).toBe(false);
+    });
+
+    it('should return false given an empty array', function () {
+      expect(_isFalse([])).toBe(false);
+    });
+
+    it('should return false given an empty object', function () {
+      expect(_isFalse({})).toBe(false);
     });
   });
 });
