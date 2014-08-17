aece0a78a20e7a155d5fc0598bacef625327f0e6
Ryan Albon <ryan@honkmobile.com>
2014-08-17 11:55:40 -0400

    More tests for equals function

--- tests/equals.js
+++ tests/equals.js
@@ -17,14 +17,36 @@ describe('hm.lib.predicate.Predicate', function () {
     });
 
     it('should work with strings', function () {
-      var i, string, _isSameString;
+      var i, string, differentString, _isSameString;
 
       for (i = 0; i < 10000; i++) {
-        string        = Math.random().toString();
-        _isSameString = _equals(string);
+        string          = Math.random().toString();
+        differentString = string + 'some suffix';
+        _isSameString   = _equals(string);
 
         expect(_isSameString(string)).toBe(true);
+        expect(_isSameString(differentString)).toBe(false);
       }
+    });
+
+    it('should work with numbers', function () {
+      var i, number, differentNumber, _isSameNumber;
+
+      for (i = 0; i < 10000; i++) {
+        number          = Math.random();
+        differentNumber = number + Math.random();
+        _isSameNumber   = _equals(number);
+
+        expect(_isSameNumber(number)).toBe(true);
+        expect(_isSameNumber(differentNumber)).toBe(false);
+
+        _isSameNumber = _equals(-number);
+
+        expect(_isSameNumber(-number)).toBe(true);
+        expect(_isSameNumber(differentNumber)).toBe(false);
+      }
+
+      expect(_equals(0)(0)).toBe(true);
     });
   });
 });
