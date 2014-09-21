0d67ff7d3dfe80c7427979e4778fa8d722dba229
Ryan Albon <ryan@honkmobile.com>
2014-09-20 23:32:41 -0400

    Configuring karma coverage

--- karma.conf.js
+++ karma.conf.js
@@ -32,13 +32,21 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      './src/*.js': ['coverage']
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['coverage'],
+
+
+    coverageReporter: {
+      type: 'lcovonly',
+      dir: 'coverage',
+      subdir: '.'
+    },
 
 
     // web server port
